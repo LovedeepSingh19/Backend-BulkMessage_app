69edcb0a4812ad1750a551c5abf8b393732a8309
Lovedeep <gurugobind.sh@gmail.com>
2023-07-15 20:36:27 +0530

    cors 2

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ require("dotenv").config();
 
 const port = 8080 || process.env.PORT;
 
-var corsOptions = {
-  origin: 'https://vercel.live/link/backend-bulk-message-app-git-main-lovedeepsingh19.vercel.app?via=deployment-domains-list-branch',
-  optionsSuccessStatus: 200
-}
+// var corsOptions = {
+//   origin: 'https://vercel.live/link/backend-bulk-message-app-git-main-lovedeepsingh19.vercel.app?via=deployment-domains-list-branch',
+//   optionsSuccessStatus: 200 // some 
+// }
  
 // app.get('/products/:id', cors(corsOptions), function (req, res, next) {
 //   res.json({msg: 'This is CORS-enabled for only example.com.'})
@@ -23,7 +23,13 @@ mongoose
   .then(() => console.log("Connected to mongo `Successful"))
   .catch((err) => console.log("error " + err));
 
-app.use(cors(corsOptions));
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", 'https://vercel.live/link/backend-bulk-message-app-git-main-lovedeepsingh19.vercel.app?via=deployment-domains-list-branch'); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(require("./routes/routes"));
