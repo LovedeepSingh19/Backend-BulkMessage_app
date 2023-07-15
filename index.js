a90bc98d467a7b71d910b280ebfa869be3a3d5ed
Lovedeep <gurugobind.sh@gmail.com>
2023-07-15 20:30:15 +0530

    cors

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ require("dotenv").config();
 
 const port = 8080 || process.env.PORT;
 
+var corsOptions = {
+  origin: 'https://vercel.live/link/backend-bulk-message-app-git-main-lovedeepsingh19.vercel.app?via=deployment-domains-list-branch',
+  optionsSuccessStatus: 200
+}
+ 
+// app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for only example.com.'})
+// })
+
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -14,7 +23,7 @@ mongoose
   .then(() => console.log("Connected to mongo `Successful"))
   .catch((err) => console.log("error " + err));
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(require("./routes/routes"));
