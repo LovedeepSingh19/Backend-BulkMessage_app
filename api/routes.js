2a1dc86d403a65dbaa68cdd5e67160a500a17e84
Lovedeep <gurugobind.sh@gmail.com>
2023-07-18 09:32:22 +0530

    time

--- api/routes.js
+++ api/routes.js
@@ -166,7 +166,7 @@ const clients = new Client();
       setTimeout(() => {
         clients.destroy();
         console.log("Client destroyed!");
-      }, 30000);
+      }, 80000);
 
       clients.initialize();
     }
